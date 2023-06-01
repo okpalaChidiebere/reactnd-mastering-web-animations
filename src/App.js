04f1a00dc42c65fffa929bcfbc0290d1ead2940f
okpalaChidiebere <okpalacollins4@gmail.com>
2023-06-01 11:11:25 -0600

    initial code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Home from "./components/Home";
+import AnimatedColorPicker from "./components/AnimatedColorPicker";
 import styles from "./css/App.module.css";
 
 function App() {
@@ -8,7 +14,9 @@ function App() {
       <div className={styles.App}>
         <Routes>
           {/* <Route element={Layout}> */}
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate to="/animatedColorPicker" />} />
+          <Route path="home" element={<Home />} />
+          <Route path="animatedColorPicker" element={<AnimatedColorPicker />} />
           {/* </Route> */}
         </Routes>
       </div>
---
+++ src/components/AnimatedColorPicker.js
@@ -0,0 +1,6 @@
+import styles from "../css/styles.module.css";
+
+export default function AnimatedColorPicker() {
+  const { container } = styles;
+  return <div className={container}></div>;
+}
